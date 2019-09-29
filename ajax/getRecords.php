b64b73c0ca7a32c1ca006201b36126659fe3c997
cobol1962 <cobol1962@gmail.com>
2019-09-29 07:30:21 +0200

    29.09

--- ajax/getRecords.php
+++ ajax/getRecords.php
@@ -1,7 +1,7 @@
 <?php
 	include($_SERVER["DOCUMENT_ROOT"] . "/include/database.php");
   $i = 0;
-  $qvr = "SELECT `id`, `category`, `name`, `html`, `LitRank`, `OverallVotes` FROM `cards` order by (`OverallVotes`) DESC";
+  $qvr = "SELECT `id`, `category`, `name`, `html`, `LitRank`, `OverallVotes`,`spotify`,`apple` FROM `cards` order by (`OverallVotes`) DESC";
   $rez = $mysqli->query($qvr);
   $i = 1;
   $rz = "";
@@ -11,8 +11,14 @@
         $rz .= "<div class='col l4 m12 s12 gallery-item gallery-expand gallery-filter polygon " . $c1 . "' artsearch_m='" . str_replace(" ","_", $row["name"]) . "' artname='" . $row['name'] . "' cardid='" . $row["id"] . "' order='" . $i . "' category='" . $row['category'] . "'>
             <div class='gallery-curve-wrapper' pageid='" . $row['name'] . "'>"
                     . $row["html"] . "
-                  </div>
-            </div><!--endartist-->";
+                  </div>";
+						if ($row["spotify"] != "") {
+							$rz .= "<span spotify>" . $row["spotify"] . "</span>";
+						}
+						if ($row["apple"] != "") {
+							$rz .= "<span apple>" . $row["apple"] . "</span>";
+						}
+            $rz .= "</div><!--endartist-->";
       $i++;
     }
     echo $rz;
--- share.php
+++ share.php
@@ -1,18 +1,48 @@
 <?php
 
-  header('Location: ' . "http://goat100.com/#" . $_GET["artsearch"]);
-include($_SERVER["DOCUMENT_ROOT"] . "/include/database.php");
-$res = [];
-$qvr = "select * from `cards`  where `id`='" . $_GET["id"] . "' limit 1";
+    include($_SERVER["DOCUMENT_ROOT"] . "/include/database.php");
+    $res = [];
+    $qvr = "select * from `cards`  where `id`='" . $_GET["id"] . "' limit 1";
 
-$result = $mysqli->query($qvr);
-$rw = mysqli_fetch_assoc($result);
+    $result = $mysqli->query($qvr);
+    $rw = mysqli_fetch_assoc($result);
+    $doc = new DOMDocument();
+    $doc->loadHTML($rw["html"]);
+    $items = $doc->getElementsByTagName('img');
+   $it = $doc->getElementsByTagName('p');
+   $desc = "";
+   foreach ($it as $t)
+  {
+
+    $desc .= ($t->nodeValue != "") ? $t->nodeValue . " " : "";
+
+  }
  ?>
 <!DOCTYPE html>
 <head>
   <title>GOAT100 - <?=$rw["name"]?></title>
+  <meta property="og:title" content="GOAT100 - <?=$rw["name"]?>">
+  <meta property="og:description" content="<?=$desc?>">
+  <meta property="og:image" content="https://goat100.com/<?=$items[0]->getAttribute("src")?>?<?=time()?>">
+  <meta property="og:url" content="https://goat100.com/share.php?id=<?=$_GET["id"]?>&amp;artsearch=<?=$_GET["artsearch"]?>">
+  <meta property="og:image:width" content="300"/>
+  <meta property="og:image:height" content="330"/>
+
+  <meta name="twitter:title" content="GOAT100 - <?=$rw["name"]?>">
+  <meta name="twitter:description" content="<?=$desc?>">
+  <meta name="twitter:image" content="https://goat100.com/<?=$items[0]->getAttribute("src")?>">
+  <meta name="twitter:card" content="summary_large_image">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
-  <?=$rw["html"]?>
+  <img src="https://goat100.com/<?=$items[0]->getAttribute("src")?>?<?=time()?>" />
+  <br />
+  <p><?=$desc?></p>
 </body>
+<script type="text/javascript">
+  $(document).ready(function() {
+    window.location.href = "http://goat100.com/#<?=$_GET["artsearch"]?>";
+  });
+</script>
 </html>
+<?php  ?>
